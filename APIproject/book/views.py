9f1b73e250893c949f2f498d476140fb95640df1
arch <karnriki70@gmail.com>
2024-12-18 16:58:14 +0545

    1st commit of making API system using Django.

---
+++ APIproject/book/views.py
@@ -0,0 +1,24 @@
+from .models import Book
+from .serializer import BookSerializer
+from rest_framework import generics, permissions
+
+# Create your views here.
+# CRUD application operations
+# List -> GET request
+# Create -> POST request
+# Retrieve -> GET request
+# Update -> PUT request
+# Delete -> DELETE request
+
+class ListBook(generics.ListAPIView):
+    """Lists all books on the database"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permissions = [permissions.AllowAny]
+    
+class CreateBook(generics.CreateAPIView):
+    """Create a new book entry"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permissions = [permissions.AllowAny]
+    
